8f92c0b0d00862c5624a2398dbe2b6b808a9e5d9
swansea <swansea2013@naver.com>
2024-01-15 00:18:12 +0900

    2장 완료

--- src/pages/todo/IndexPage.jsx
+++ src/pages/todo/IndexPage.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, {useCallback} from "react";
+import {Outlet, useNavigate} from "react-router-dom";
 import BasicLayout from "../../layout/BasicLayout";
-import {Link, Outlet} from "react-router-dom";
 
 const IndexPage = () => {
+  const navigate = useNavigate();
+
+  const handleClickList = useCallback(() => {
+    navigate({pathname: "list"});
+  });
+
+  const handleClickAdd = useCallback(() => {
+    navigate({pathname: "add"});
+  });
+
   return (
     <BasicLayout>
       <div className="w-full flex m-2 p-2">
-        <div className="text-xl m-1 p-2 w-20 font-extrabold text-center underline">
-          <Link to="list">LIST</Link>
+        <div
+          className="text-xl m-1 p-2 w-20 font-extrabold text-center underline cursor-pointer"
+          onClick={handleClickList}
+        >
+          LIST
         </div>
-        <div className="text-xl m-1 p-2 w-20 font-extrabold text-center underline">ADD</div>
+        <div
+          className="text-xl m-1 p-2 w-20 font-extrabold text-center underline cursor-pointer"
+          onClick={handleClickAdd}
+        >
+          ADD
+        </div>
         <div className="flex flex-wrap w-full"></div>
         <Outlet />
       </div>
---
+++ src/pages/todo/ReadPage.jsx
@@ -0,0 +1,40 @@
+import {useCallback} from "react";
+import {createSearchParams, useNavigate, useParams, useSearchParams} from "react-router-dom";
+
+const ReadPage = () => {
+  const {tno} = useParams();
+  const navigate = useNavigate();
+
+  const [queryParams] = useSearchParams();
+
+  const page = queryParams.get("page") ? parseInt(queryParams.get("page")) : 1;
+  const size = queryParams.get("size") ? parseInt(queryParams.get("size")) : 10;
+
+  const queryStr = createSearchParams({page, size}).toString();
+
+  const moveToModify = useCallback(
+    tno => {
+      navigate({pathname: `/todo/modify/${tno}`, search: queryStr});
+    },
+    [tno, page, size]
+  );
+
+  const moveToList = useCallback(
+    todo => {
+      navigate({pathname: `/todo/list`, search: queryStr});
+    },
+    [page, size]
+  );
+
+  return (
+    <div className="text-3xl font-extrabold">
+      Todo Read page component {tno}
+      <div>
+        <button onClick={() => moveToModify(tno)}>Test Modify</button>
+        <button onClick={() => moveToList()}>Move to List</button>
+      </div>
+    </div>
+  );
+};
+
+export default ReadPage;
