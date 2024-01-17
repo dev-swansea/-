725a894b626c81ee37a742b2ceb7aa4756dfea13
swansea <swansea2013@naver.com>
2024-01-17 19:16:07 +0900

    3장 시작

---
+++ src/main/java/com/learn/learnreact/LearnReactApplication.java
@@ -0,0 +1,13 @@
+package com.learn.learnreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnReactApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LearnReactApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/learn/learnreact/LearnReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.learnreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnReactApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
